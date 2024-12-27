6f54c8cb513c69373c9025f451953b9e8690ac8f
Sable Ayala <sable262021@gmail.com>
2024-12-27 15:38:33 -0500

    ✨ Added filler letter between double letters
    Insert a dummy letter (in this case "x") between any repeated letters in the string passed to the fill function. Function still needs to be cleaned up and documentation needs to be done.

--- bin/main.ml
+++ bin/main.ml
@@ -5,9 +5,30 @@ let rec split' str pos n =
     String.sub str pos n :: split' str (pos+n) n
   ;;
 
-let _split str n =
+let split str n =
   if (String.length str) mod 2 == 0 then
     split' str 0 n
   else
-    split' (str ^ String.make 1 'x') 0 n
+    split' (str ^ "x") 0 n
   ;;
+
+let rec fill' str pos =
+  let len = String.length str in
+  if pos >= len - 1 then
+    str
+  else if str.[pos] == str.[pos+1] then
+    fill' (String.sub str 0 (pos+1) ^ "x" ^ String.sub str (pos + 1) (len - (pos + 1))) 0
+  else
+    fill' str (pos + 2)
+  ;;
+
+let fill str = 
+  fill' str 0 
+  ;;
+
+let test_print key = List.iter (Printf.printf "%s ") (split key 2);;
+
+test_print "death";;
+print_endline "";;
+test_print "test";;
+print_endline (fill "teller");;
